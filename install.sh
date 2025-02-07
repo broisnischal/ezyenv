5b0443226b521305462f27b6fce6b7d2474413db
broisnischal <info@nischal-dahal.com.np>
2025-02-07 11:55:09 +0545

    feat:publ-installsh

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+curl -fsSL https://github.com/broisnischal/ezyenv/releases/latest/download/ezyenv -o /usr/local/bin/ezyenv
+chmod +x /usr/local/bin/ezyenv
+echo "ezyenv installed successfully!"
