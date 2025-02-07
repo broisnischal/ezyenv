35b135e1800172cd6fbe9f58d9abc3844eab7593
broisnischal <info@nischal-dahal.com.np>
2025-02-07 11:57:41 +0545

    feat:publish-change

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
-sudo curl -fsSL https://github.com/broisnischal/ezyenv/releases/latest/download/ezyenv -o /usr/local/bin/ezyenv
-sudo chmod +x /usr/local/bin/ezyenv
-echo "ezyenv installed successfully!"
+
+TARGET_DIR="/usr/local/bin"
+
+echo "Downloading ezyenv..."
+curl -fsSL https://github.com/broisnischal/ezyenv/releases/latest/download/ezyenv -o "$TARGET_DIR/ezyenv"
+
+if [ $? -ne 0 ]; then
+  echo "Download failed. Check the URL or permissions."
+  exit 1
+fi
+
+chmod +x "$TARGET_DIR/ezyenv"
+echo "ezyenv installed successfully! Run 'ezyenv' to use it."
